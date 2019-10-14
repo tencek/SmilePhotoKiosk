9d4fa79e9190f876d3c0333f08e14ccd92e1bfb2
Pavel Kučera <pavel.kucera@edhouse.cz>
2019-10-14 10:04:24 +0200

    picturefolderwatcherscript imrpovements

--- PictureFolderWatcherScript/PictureFolderWatcherScript.ps1
+++ PictureFolderWatcherScript/PictureFolderWatcherScript.ps1
@@ -3,16 +3,22 @@
 # Based on https://mcpmag.com/articles/2019/05/01/monitor-windows-folder-for-new-files.aspx
 #
 
+$PosPrinter = "TM-T88VMU"
+$Pos4NetImagePrinter = "C:\Users\pk250193\source\repos\tencek\Pos4NetImagePrinter\src\Pos4NetImagePrinter\bin\Debug\net472\Pos4NetImagePrinter.exe"
+$pictureFolderPath = [environment]::GetFolderPath("mypictures")
+
+#######################################################
+
+Set-Alias -Name Pos4NetImagePrinter -Value $Pos4NetImagePrinter
+
 # Unregister any events registered in this session. Comment out if it is not what you want.
 Get-EventSubscriber | Unregister-Event
-
-$pictureFolderPath = [environment]::GetFolderPath("mypictures")
 
 $onNewPictureCreated =
 {
     $newPicturePath = $event.SourceEventArgs.FullPath
     Write-Host "New picture $newPicturePath created!"
-    C:\Users\Manžel\source\repos\Pos4NetImagePrinter\src\Pos4NetImagePrinter\bin\Debug\net472\Pos4NetImagePrinter.exe /width Full /FileNameAsLabel /Conversion bmp32 /printer "TM-T88VMU" /path "$newPicturePath"
+    Pos4NetImagePrinter /width Full /FileNameAsLabel /Conversion bmp32 /printer $PosPrinter /path "$newPicturePath"
 }
 
 $pictureFolderWatcher = New-Object System.IO.FileSystemWatcher
